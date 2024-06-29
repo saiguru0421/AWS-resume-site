48443fb240da72e7dc7a1d5e36e4dac76c55a85a
saiguru0421 <saiguru0421@gmail.com>
2024-06-29 12:15:35 -0400

    updated data

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,16 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     navItems.forEach(item => {
         item.addEventListener('click', function () {
-            // Remove active class from all nav items
+           
             navItems.forEach(nav => nav.classList.remove('active'));
 
-            // Add active class to the clicked nav item
+           
             item.classList.add('active');
 
-            // Hide all project details
+            
             projectDetails.forEach(detail => detail.classList.remove('active'));
 
-            // Show the corresponding project detail
             const targetId = item.getAttribute('data-target');
             document.getElementById(targetId).classList.add('active');
         });
